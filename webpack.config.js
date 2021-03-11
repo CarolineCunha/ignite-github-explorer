8f212ffd8a5f9c5514937dfb92232f578f889bf2
CarolineCunha <caroline.cunha.b@hotmail.com>
2021-03-11 07:10:52 -0300

    webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 module.exports = {
+    mode: 'development',
     entry: path.resolve(__dirname, 'src', 'index.jsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -8,6 +10,14 @@ module.exports = {
     resolve: {
         extensions: ['.js','.jsx']
     },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+    },
+    plugins:[
+        new HtmlWebPackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html')
+        })
+    ],
 
     module: {
         rules: [

6670c1a0d2758105726968724714794860381a68
CarolineCunha <caroline.cunha.b@hotmail.com>
2021-03-15 07:39:38 -0300

    fast refresh

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
-const HtmlWebPackPlugin = require('html-webpack-plugin')
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const isDevelopment = process.env.NODE_ENV != 'production';
+
 
 module.exports = {
     mode: isDevelopment ? 'development': 'production',
@@ -15,19 +17,28 @@ module.exports = {
     },
     devServer: {
         contentBase: path.resolve(__dirname, 'public'),
+        hot: true,
     },
     plugins:[
+        isDevelopment && new ReactRefreshWebpackPlugin(),
         new HtmlWebPackPlugin({
             template: path.resolve(__dirname, 'public', 'index.html')
         })
-    ],
+    ].filter(Boolean),
 
     module: {
         rules: [
             {
                 test:/\.jsx$/,
                 exclude: /node_modules/,
-                use: 'babel-loader'
+                use: { 
+                    loader: 'babel-loader',
+                    options: {
+                        plugins: [
+                            isDevelopment && require.resolve('react-refresh/babel')
+                        ].filter(Boolean),
+                    }
+                }
             },
             {
                 test:/\.scss$/,
